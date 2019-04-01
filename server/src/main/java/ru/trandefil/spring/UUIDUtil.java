24332b3e76a1489f39ecc6a62372b25c791f579f
trandefil <trandafir.iura@gmail.com>
2019-04-01 17:43:09 +0300

    jsf integration complete

--- server/src/main/java/ru/trandefil/spring/UUIDUtil.java
+++
@@ -1,9 +0,0 @@
-package ru.trandefil.spring;
-
-import java.util.UUID;
-
-public class UUIDUtil {
-    public static String getUniqueString(){
-        return UUID.randomUUID().toString();
-    }
-}
