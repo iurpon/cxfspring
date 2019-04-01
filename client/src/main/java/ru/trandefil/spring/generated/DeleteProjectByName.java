af232c972f47241d20a135b72b88409a4c7103cd
trandefil <trandafir.iura@gmail.com>
2019-04-01 18:35:23 +0300

    end point generate

---
+++ client/src/main/java/ru/trandefil/spring/generated/DeleteProjectByName.java
@@ -0,0 +1,87 @@
+
+package ru.trandefil.spring.generated;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for deleteProjectByName complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="deleteProjectByName"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="projectName" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="session" type="{http://generated.spring.trandefil.ru/}session" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "deleteProjectByName", propOrder = {
+    "projectName",
+    "session"
+})
+public class DeleteProjectByName {
+
+    protected String projectName;
+    protected Session session;
+
+    /**
+     * Gets the value of the projectName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getProjectName() {
+        return projectName;
+    }
+
+    /**
+     * Sets the value of the projectName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setProjectName(String value) {
+        this.projectName = value;
+    }
+
+    /**
+     * Gets the value of the session property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Session }
+     *     
+     */
+    public Session getSession() {
+        return session;
+    }
+
+    /**
+     * Sets the value of the session property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Session }
+     *     
+     */
+    public void setSession(Session value) {
+        this.session = value;
+    }
+
+}
