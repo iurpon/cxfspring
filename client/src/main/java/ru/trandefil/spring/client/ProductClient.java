9bac25c1c88ca7101d6a89a5ed707d4443a850b8
trandefil <trandafir.iura@gmail.com>
2019-04-02 12:10:42 +0300

    refactor

--- client/src/main/java/ru/trandefil/spring/client/ProductClient.java
+++ client/src/main/java/ru/trandefil/spring/client/ProductClient.java
@@ -3,8 +3,6 @@ package ru.trandefil.spring.client;
 import ru.trandefil.spring.endpoint.ProjectEndPointImplService;
 import ru.trandefil.spring.endpoint.UserEndPointImplService;
 import ru.trandefil.spring.generated.*;
-import ru.trandefil.spring.service.Product;
-import ru.trandefil.spring.service.ProductService;
 
 import java.util.List;
 
@@ -12,17 +10,9 @@ public class ProductClient {
 
     public static void main(String[] args) {
         try {
-            ProductService productService = new ru.trandefil.spring.service.impl.ProductService().getProductServiceImplPort();
-            Product product = productService.getProduct(101);
-            System.out.println(product.getProductId());
-            System.out.println(product.getProductName());
-            System.out.println(product.getProductCatg());
-            List<Product> list = productService.getAllProducts();
-            list.forEach(System.out::println);
-
-            UserEndPoint userEndPoint =  new UserEndPointImplService().getUserEndPointImplPort();
+            UserEndPoint userEndPoint = new UserEndPointImplService().getUserEndPointImplPort();
             Session session = userEndPoint.getSession("root", "root");
-            if(session != null){
+            if (session != null) {
                 List<UserDTO> allUsers = userEndPoint.getAllUsers(session);
                 allUsers.forEach(System.out::println);
             }
@@ -31,7 +21,7 @@ public class ProductClient {
             List<ProjectDTO> projects = projectEndPoint.getAllProjects(session);
             projects.forEach(System.out::println);
 
-
+//            TaskEndPoint
 
         } catch (Exception e) {
             e.printStackTrace();
--- client/src/main/java/ru/trandefil/spring/service/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.spring.trandefil.ru/")
-package ru.trandefil.spring.service;
