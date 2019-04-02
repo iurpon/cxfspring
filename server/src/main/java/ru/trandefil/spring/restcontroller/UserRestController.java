4c4d60d9e1276dc5390383436550734bb6f1e73f
trandefil <trandafir.iura@gmail.com>
2019-04-02 18:03:35 +0300

    rest?

---
+++ server/src/main/java/ru/trandefil/spring/restcontroller/UserRestController.java
@@ -0,0 +1,40 @@
+package ru.trandefil.spring.restcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.trandefil.spring.model.User;
+import ru.trandefil.spring.service.UserService;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
+public class UserRestController {
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    @Autowired
+    private UserService userService;
+
+    @PostConstruct
+    public void post(){
+        logger.info("========================== post construct userService : " +  userService);
+    }
+
+    @GetMapping(value = "/rest",produces = MediaType.APPLICATION_JSON_VALUE)
+    public String hello(){
+        logger.info("=========================== user rest controller hello");
+        return "hello";
+    }
+
+
+    @GetMapping(value = "/rest/users",produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<User> getUsers(){
+        logger.info("=========================== user rest controller get users");
+        return userService.getAll();
+    }
+
+}
---
+++ server/src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Добро пожаловать, JSP!</title>
+    <link rel="stylesheet" type="text/css" href="resources/css/menu.css">
+</head>
+<body>
+<h1>Добро пожаловать!</h1>
+
+<ul class="menu">
+    <li><a class="menu" href="projects">See all Project</a></li>
+    <li><a class="menu" href="tasks">See all Tasks</a></li>
+    <!--<li><a class="menu" href="/logout">Logout</a></li>-->
+    <li><a class="menu" href="users">See all Users</a></li>
+</ul>
+
+</body>
+</html>
