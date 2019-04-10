22e9bb7af7ef76a1f4deac971c64b6ffac263c9f
trandefil <trandafir.iura@gmail.com>
2019-04-10 15:14:30 +0300

    refactor

--- server/src/main/java/ru/trandefil/spring/resource/RootController.java
+++ server/src/main/java/ru/trandefil/spring/resource/RootController.java
@@ -1,13 +1,9 @@
 package ru.trandefil.spring.resource;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.trandefil.spring.LoggedUser;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import java.util.logging.Logger;
 
 @Controller
@@ -27,17 +23,8 @@ public class RootController {
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
-
-/*    @GetMapping("/logout")
-    public String logout(HttpServletRequest request, HttpServletResponse response){
-        logger.info(":::::logout");
-*//*        HttpServletResponse response1 = response;
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        new SecurityContextLogoutHandler().logout(request, response1,authentication);*//*
-        return "login";
-    }*/
 
 }
